76ec90599776c8d002da31a2fc56fd5b0fa2f8b5
ayush <ayushahirrao@gmail.com>
2023-01-31 21:28:02 +0530

    added compiler bot (c/c++, python, java)

--- bots/Bots.js
+++ bots/Bots.js
@@ -1,9 +1,23 @@
-const http = require('http');
-const express = require('express');
+// calculator
 const math = require('mathjs');
+
+// cpp
 const {
-    resourceLimits
-} = require('worker_threads');
+    CppCompile
+} = require('../compiler/cpp');
+
+// java
+const {
+    javaCompile
+} = require('../compiler/java');
+
+// python
+const {
+    PythonCompile
+} = require('../compiler/python');
+
+
+const express = require('express');
 const app = express();
 
 app.use(function (req, res, next) {
@@ -25,6 +39,64 @@ app.get('/calculate', async (req, res) => {
         res.status(500).send(error.message);
     }
 });
+
+// compiler bot
+app.get('/compiler', async (req, res) => {
+    let lang = decodeURIComponent(req.query.lang);
+    let code = decodeURIComponent(req.query.code);
+    let input = decodeURIComponent(req.query.input);
+
+    console.log(lang, code, input);
+    try {
+        // cpp
+        if (lang === 'cpp') {
+            await CppCompile(code, input)
+                .then(result => {
+                    res.send(JSON.stringify({
+                        stdout: result.stdout,
+                        stderr: result.stderr,
+                        statusMes: result.statusMes
+                    }));
+                })
+                .catch(error => {
+                    console.error(error)
+                });
+        }
+        // Java
+        else if (lang === 'java') {
+            await javaCompile(code, input)
+                .then(result => {
+                    res.send(JSON.stringify({
+                        stdout: result.stdout,
+                        stderr: result.stderr,
+                        statusMes: result.statusMes
+                    }));
+                })
+                .catch(error => {
+                    console.error(error)
+                });
+        }
+        // Python
+        else if (lang === 'python') {
+            await PythonCompile(code, input)
+                .then(result => {
+                    res.send(JSON.stringify({
+                        stdout: result.stdout,
+                        stderr: result.stderr,
+                        statusMes: result.statusMes
+                    }));
+                })
+                .catch(error => {
+                    console.error(error)
+                });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+
+});
+
 
 app.listen(7774, () => {
     console.log('Math API listening on port 7774!');
